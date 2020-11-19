b2ab55575ec6697d34a60624309bfeab0ad2fec1
Aner Lucero <4rgento@gmail.com>
2020-11-19 13:51:18 -0300

    Add nix file

---
+++ adventOfCode2019.nix
@@ -0,0 +1,20 @@
+{ mkDerivation, array, attoparsec, base, doctest, lens, mtl
+, recursion-schemes, stdenv, text, vector
+}:
+mkDerivation {
+  pname = "adventOfCode2019";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array attoparsec base doctest lens mtl recursion-schemes text
+    vector
+  ];
+  executableHaskellDepends = [
+    array attoparsec base lens mtl text vector
+  ];
+  testHaskellDepends = [ base ];
+  description = "Advent of code 2019";
+  license = stdenv.lib.licenses.bsd3;
+}
