88fa85f810a78d49f68b0359b5f1196d8070bbad
Aner Lucero <4rgento@gmail.com>
2020-11-19 16:20:07 -0300

    Refactored Day01

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import qualified Day02 (printResult)
+import qualified Day01
+
+import qualified Data.ByteString as BS
+import Data.Attoparsec.ByteString (Parser)
+import qualified Data.Attoparsec.ByteString as DAB
+
+runSolution :: Show b => (Parser a, (a -> b, a -> b)) -> IO ()
+runSolution (parser, (solver1, solver2)) = do
+	contents <- BS.getContents
+	let parseResult = DAB.parseOnly parser contents
+	case parseResult of
+		Left err -> putStrLn err
+		Right input -> do
+			print $ solver1 input
+			print $ solver2 input
 
 main :: IO ()
-main = Day02.printResult
+main = runSolution Day01.solution
--- src-lib/Day01.hs
+++ src-lib/Day01.hs
@@ -1,32 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Day01 (someFunc) where
+module Day01 (solution) where
+
+import qualified Data.ByteString as BS
+import Data.Attoparsec.ByteString (Parser)
+import qualified Data.Attoparsec.ByteString as DAB
+import Data.Attoparsec.ByteString.Char8 (digit, endOfLine)
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.List as DL
 
---input :: String
---input = unlines ["12", "14", "1969", "100756"]
+solution :: (Parser [Int], ([Int] -> Int, [Int] -> Int))
+solution = (parseInput, (solver1, solver2))
 
-someFunc :: IO ()
-someFunc = parseInput >>= print .
-		DL.foldl' (+) 0 .
-		map fuelPerModule
+-- | Generates the solution from the input
+solver2 :: [Int] -> Int
+solver2 = DL.foldl' (+) 0 . fmap fuelPerModule
 
--- hylomorphims
--- hylo :: Functor f => (f b -> b) -> (a -> f a) -> a -> b
+solver1 :: [Int] -> Int
+solver1 = DL.foldl' (+) 0 . fmap fuelForMass
+
+-- | Computes the fuel needed by a module
+--
+-- It takes into account mass of the fuel
+-- >>> fuelPerModule 14
+-- 2
+-- >>> fuelPerModule 100756
+-- 50346
 fuelPerModule :: Int -> Int
-fuelPerModule x = DL.foldl' (+) 0 $
-	filter (>0) $
-	DL.unfoldr unfolder x
+fuelPerModule = DL.foldl' (+) 0 .
+	filter (>0) .
+	DL.unfoldr unfolder
 	where
 	unfolder :: Int -> Maybe (Int, Int)
 	unfolder x
 		| x <= 0 = Nothing
 		| otherwise = Just (fuelForMass x, fuelForMass x)
 
-parseInput :: IO [Int]
-parseInput = map (read :: String -> Int) <$> (lines <$> getContents)
+parseInput :: Parser [Int]
+parseInput = 
+	fmap (read :: String -> Int) . filter (not . null) <$>
+		DAB.many' digit `DAB.sepBy` endOfLine
 
 -- | The amount of fuel per unit of mass
 --
@@ -40,9 +54,3 @@ parseInput = map (read :: String -> Int) <$> (lines <$> getContents)
 -- 33583
 fuelForMass :: Int -> Int
 fuelForMass x = x `div` 3 - 2
-
-
-
-
-
-
